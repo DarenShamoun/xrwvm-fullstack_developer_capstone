ee8d4180a84fdeede70db5cffd1eeea1e1cae61f
Daren Shamoun <darenshamoun@gmail.com>
2024-08-29 22:27:02 -0400

    Implemented CarMake and CarModel models, added admin site registration, and fixed get_cars view.

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Allows adding one more CarModel entry at a time
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'type', 'year']
+    search_fields = ['name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]  # Displays CarModelInline within CarMake admin page
+    list_display = ['name', 'description']
+    search_fields = ['name']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,11 @@ urlpatterns = [
     #path for logout
     path('logout', views.logout_request, name='logout'),
 
-
     # path for dealer reviews view
 
     # path for add a review view
+
+    # path to get cars
+    path('get_cars', views.get_cars, name='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
