4999d26553ca8f2aeab8c3bc926f4652ca46f090
darenshamoun <darenshamoun@gmail.com>
2024-08-30 20:26:25 -0400

    adressed linter issues

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1  # Allows adding one more CarModel entry at a time
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'car_make', 'type', 'year']
     search_fields = ['name']
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
@@ -17,6 +20,7 @@ class CarMakeAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
     search_fields = ['name']
 
+
 # Register models here
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,3 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
@@ -16,6 +14,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # Car Model model
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
@@ -27,13 +26,14 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
     # You can add more fields as necessary
 
     def __str__(self):
         return f'{self.car_make.name} {self.name}'  # Return the make and model as the string representation
-
